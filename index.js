c757068b379ceb4b03171541444186c8e35eee69
rzeee <rameez007rz@gmail.com>
2023-10-24 15:41:14 +0530

    done basic search

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
-exports.hello=(name)=>{
-    console.log(`Hello ${name}...`);
+const { MongoClient } = require('mongodb');
+
+async function searchInDatabase(searchKeyword, databaseURL, collectionName) {
+    try {
+        const client = new MongoClient(databaseURL, { useNewUrlParser: true, useUnifiedTopology: true });
+        await client.connect();
+
+        const db = client.db();
+        const collection = db.collection(collectionName); 
+
+        const results = await collection.find({
+            $text: { $search: searchKeyword }
+        }).toArray();
+
+        client.close();
+        return results;
+    } catch (error) {
+        throw new Error(`Error searching in the database: ${error.message}`);
+    }
 }
+
+module.exports = {searchInDatabase};
